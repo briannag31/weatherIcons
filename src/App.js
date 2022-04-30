5bce5b4c61bdfc3c33b4ab8029c2412c303f9a47
Brianna Gaines <briannagaines31@gmail.com>
2022-04-30 17:40:50 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
 import "./styles.css";
+import "./weatherData.js"
+import weatherData from "./weatherData.js";
+import WeatherForecast from "./WeatherForecast";
 
 export default function App() {
+  const allWeatherData = weatherData.map((ele, index) =>{
+    return <WeatherForecast img={ele.img} condtions={ele.conditions} time={ele.time} key={index}/> 
+  })
   return (
-    <div className="App">
-      <h1>My WeatherIcons App</h1> 
-    </div>
-  );
+    <div className = "App">
+      <section>
+      {allWeatherData}
+      </section>
+    </div> 
+    
+  )
 }
---
+++ src/weatherData.js
@@ -0,0 +1,29 @@
+const weatherData = [
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/day.svg",
+        conditions: "Sunny",
+        time: "day",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/night.svg",
+        conditions: "Clear",
+        time: "day",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/stormy.svg",
+        conditions: "Clear",
+        time: "day",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-day_t7ckxp.svg",
+        conditions: "Partly Sunny",
+        time: "night",
+    },
+    {
+        img: "http://res.cloudinary.com/jkeohan/image/upload/v1535732381/cloudy-night.svg",
+        conditions: "Clear",
+        time: "day",
+    },
+]
+
+export default weatherData
